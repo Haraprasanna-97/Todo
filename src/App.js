490734a0645ff08a174d4427dc7b267ed51d2d5f
Haraprasanna Misra <haraprasannamisra@gmail.com>
2023-09-11 10:22:17 +0530

    Made changes to the directory structure of the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Home from "./components/Home"
 import About from "./components/About"
 import Contact from "./components/Contact"
 import Navbar from "./components/Navbar"
-import ShoppingCart from "./components/features/ShopingCart"
-import Todo from './components/features/Todo';
+import ShoppingCart from "./components/features/ShopingCart/ShopingCart"
+import Todo from './components/features/Todo/Todo';
 
 function App() {
   return (
